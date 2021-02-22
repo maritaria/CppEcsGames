cc7467de1cad21a0240943d02250d23bfcc27889
BuildTools <unconfigured@null.spigotmc.org>
2021-02-22 21:04:52 +0100

    Implement exercise 1 and structure project for next exercise

---
+++ exercises.h
@@ -0,0 +1,5 @@
+#pragma once
+
+int main_exercise1();
+int main_exercise2();
+// int main_exercise3();
---
+++ program.cpp
@@ -0,0 +1,10 @@
+// CppHelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "exercises.h"
+
+int main()
+{
+	return main_exercise1();
+}
